39dae8baa1c5cd8160d31948f63b00aaa481f799
fredkendy <fredkendy9@gmail.com>
2024-01-23 14:47:39 -0300

    barra de navegacao

---
+++ lib/screens/calendar_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CalendarScreen extends StatefulWidget {
+  const CalendarScreen({super.key});
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Calendar Screen"),
+      )
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:employee_attendance/screens/attendance_screen.dart';
+import 'package:employee_attendance/screens/calendar_screen.dart';
+import 'package:employee_attendance/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +12,63 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  //Declaring icons we'll use in thee bottom nav bar
+  List<IconData> navigationIcons = [
+    FontAwesomeIcons.solidCalendarDays,
+    FontAwesomeIcons.check,
+    FontAwesomeIcons.solidUser
+  ];
+
+  //variable responsible for containing the current page
+  int currentIndex = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+      body: IndexedStack(
+        index: currentIndex,
+        children: [
+          CalendarScreen(),
+          AttendanceScreen(),
+          ProfileScreen()
+        ],
+      ),
+
+      //BARRA DE NAVEGAÇÃO INFERIOR
+      bottomNavigationBar: Container(
+        height: 70,
+        margin: const EdgeInsets.only(left: 12, right: 12, bottom: 24),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          boxShadow: [
+            BoxShadow(color: Colors.black26, blurRadius: 10, offset: Offset(2,2))
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            for(int i = 0; i < navigationIcons.length; i++)...{
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      currentIndex = i;
+                    });
+                  },
+                  child: Center(
+                    child: FaIcon(navigationIcons[i],
+                    color: i == currentIndex ? Colors.black : Colors.grey,
+                    size: i == currentIndex ? 30 : 26,
+                    ),
+                  ),
+                ),
+              )
+            }
+          ],
+        ),
       ),
     );
   }

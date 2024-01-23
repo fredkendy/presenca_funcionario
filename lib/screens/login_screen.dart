4c38da2b23d4597c53fb47cd39cc1bd93e9edfc7
fredkendy <fredkendy9@gmail.com>
2024-01-22 21:14:23 -0300

    tela de login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,11 +8,81 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("It is working!"),
+    //variables to get width and height of device
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          Container(
+            height: screenHeight / 3,
+            width: screenWidth,
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(70)
+              )
+            ),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.qr_code_scanner, color: Colors.white, size: 80,),
+                SizedBox(height: 20,),
+                Text("Flutter Company", style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white, fontWeight: FontWeight.bold
+                ),)
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 50,),
+          Padding(padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextField(
+                decoration: const InputDecoration(
+                  label: Text("Employee Email ID"),
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder()
+                ),
+                controller: _emailController,
+              ),
+              const SizedBox(height: 20,),
+
+              TextField(
+                  decoration: const InputDecoration(
+                      label: Text("Password"),
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder()),
+                  controller: _passwordController,
+                  obscureText: true,
+                ),
+                const SizedBox(height: 30,),
+                SizedBox(
+                  height: 60,
+                  width: double.infinity,
+                  child: ElevatedButton(onPressed: () {
+
+                  }, style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)
+                      )
+                  ), child: const Text("LOGIN", style: TextStyle(fontSize: 20, color: Colors.white),),),
+                ),
+            ],
+          ),
+          )
+        ],
       ),
     );
   }

6b8e48a7e96a2358b33335ddba1dd2b05b9dd754
fredkendy <fredkendy9@gmail.com>
2024-01-29 12:13:12 -0300

    exibir historico de presenca

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,3 +1,4 @@
+import 'package:employee_attendance/models/attendance_model.dart';
 import 'package:employee_attendance/services/attendance_service.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -52,7 +53,85 @@ class _CalendarScreenState extends State<CalendarScreen> {
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if (snapshot.hasData) {
                 if (snapshot.data.length > 0) {
-                  //OUTPUT WIDGET
+                  return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                      AttendanceModel attendanceData = snapshot.data[index];
+                      return Container(
+                        margin: const EdgeInsets.only(top: 12, left: 20, right: 20, bottom: 10),
+                        height: 150,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 10,
+                              offset: Offset(2, 2)
+                            )
+                          ],
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+
+                            Expanded(child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.all(Radius.circular(20))
+                              ),
+                              child: Center(
+                                child: Text(DateFormat("EE \n dd").format(attendanceData.createdAt), style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold
+                                ),),
+                              ),
+                            )),
+
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    "Check in:",
+                                    style: TextStyle(fontSize: 20, color: Colors.black54),
+                                  ),
+                                  const SizedBox(width: 80, child: Divider(),),
+                                  Text(attendanceData.checkIn, style: const TextStyle(fontSize: 25),)
+                                ],
+                              ),
+                            ),
+
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    "Check in:",
+                                    style: TextStyle(
+                                        fontSize: 20, color: Colors.black54),
+                                  ),
+                                  const SizedBox(
+                                    width: 80,
+                                    child: Divider(),
+                                  ),
+                                  Text(
+                                    attendanceData.checkOut?.toString() ?? '--/--',
+                                    style: const TextStyle(fontSize: 25),
+                                  )
+                                ],
+                              ),
+                            ),
+                            const SizedBox(width: 15)
+                          ],
+                        ),
+                      );
+                    },
+                  );
                 } else {
                   return const Center(
                     child: Text(

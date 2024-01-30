4faa52df375223ec944cf0ed37b7e8a53b143c97
fredkendy <fredkendy9@gmail.com>
2024-01-30 14:30:55 -0300

    interface inicial da tela de perfil

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -28,14 +28,39 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
       body: dbService.userModel == null ? const Center(
         child: CircularProgressIndicator(),
-      ) : const Padding(
-        padding: EdgeInsets.all(20),
+      ) : Padding(
+        padding: const EdgeInsets.all(20),
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              
+              Container(
+                margin: const EdgeInsets.only(top: 80),
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blue
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.person,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15,),
+              Text("Employee ID: ${dbService.userModel?.employeeId}"),
+              const SizedBox(height: 30,),
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  label: Text("Full name"),
+                  border: OutlineInputBorder()
+                ),
+              )
             ],
           ),
         ),
